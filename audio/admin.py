1436a8ff08be665cf56f45f9f905f859d32eb232
Kiash254 <codewithkiash@gmail.com>
2022-09-28 13:27:20 +0300

    added models

--- audio/admin.py
+++ audio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Song
 # Register your models here.
+admin.site.site_header="Audio Panel"
+admin.site.site_title="Admin Panel"
+
+admin.site.register(Song)
--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-importos
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
